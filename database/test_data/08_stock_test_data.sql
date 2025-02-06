fc3a2d77b2654ebc4ada85c8407bd27f05647760
Martin8843 <neras012@gmail.com>
2025-02-06 10:57:31 +0100

    commit

--- database/test_data/08_stock_test_data.sql
+++
@@ -1,6 +0,0 @@
--- Zamówienie 170 dla użytkownika 331
-INSERT INTO stock (product_id, quantity) VALUES (11, 100); -- Gripex
-INSERT INTO stock (product_id, quantity) VALUES (16, 200); -- Stoperan
-INSERT INTO stock (product_id, quantity) VALUES (21, 150); -- Paracetamol
-INSERT INTO stock (product_id, quantity) VALUES (1, 50);  -- Aspirin
-INSERT INTO stock (product_id, quantity) VALUES (26, 0);  -- Septolete (out of stock)
