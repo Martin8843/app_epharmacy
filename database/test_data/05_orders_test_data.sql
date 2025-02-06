8312c0cefcb0338a039198801b4b4df5d6fdb0d1
Martin8843 <neras012@gmail.com>
2025-02-06 11:13:05 +0100

    commit

---
+++ database/test_data/05_orders_test_data.sql
@@ -0,0 +1,12 @@
+
+
+INSERT INTO orders (user_id, payment_id, order_date, order_status, total_amount, notes)
+VALUES (331, NULL, TO_DATE('2024-03-10', 'YYYY-MM-DD'), 'PENDING', 100.00, 'Test Order 1');
+
+
+INSERT INTO orders (user_id, payment_id, order_date, order_status, total_amount, notes)
+VALUES (341, NULL, TO_DATE('2025-01-10', 'YYYY-MM-DD'), 'PENDING', 160.00, 'Test Order 1');
+
+
+INSERT INTO orders (user_id, payment_id, order_date, order_status, total_amount, notes)
+VALUES (351, NULL, TO_DATE('2024-08-10', 'YYYY-MM-DD'), 'PENDING', 108.00, 'Test Order 1');
---
+++ database/test_data/06_payments_test_data.sql
@@ -0,0 +1,9 @@
+INSERT INTO payments (order_id, payment_date, payment_status, payment_method)
+VALUES (170, TO_DATE('202-06-25', 'YYYY-MM-DD'), 'COMPLETED', 'BANK_TRANSFER');
+
+INSERT INTO payments (order_id, payment_date, payment_status, payment_method)
+VALUES (180, TO_DATE('2024-05-25', 'YYYY-MM-DD'), 'COMPLETED', 'BANK_TRANSFER');
+
+
+INSERT INTO payments (order_id, payment_date, payment_status, payment_method)
+VALUES (190, TO_DATE('2024-01-25', 'YYYY-MM-DD'), 'COMPLETED', 'BANK_TRANSFER');
